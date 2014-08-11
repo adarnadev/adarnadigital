f2f9f53ce4d5e926ec36dbdb35938a104c8b5157
Agno Almario <agno@adarna.com.ph>
2014-08-11 21:20:14 +0800

    social tabs fix

--- wp-content/themes/digitheme/home.php
+++ wp-content/themes/digitheme/home.php
@@ -26,36 +26,40 @@
 	<div class="grid-12">
 			
 			<?php if(have_posts()) : while( have_posts()) : the_post();?>
-			<div id="social">
+			
+			<div class="grid-12" id="social">
 				<div><a href="https://twitter.com/share" class="twitter-share-button" data-via="twitterapi" data-lang="en">Tweet</a></div>
 				<div class="fb-like" data-href="https://developers.facebook.com/docs/plugins/" data-layout="button_count" data-action="like" data-show-faces="true" data-share="true"></div>
 			</div>
-			<h2 style="text-align:center"><?php the_title(); ?></h2>
-			
-			<p style="font-size:.7em;text-align:center;">Posted on: <?php the_time('F j, Y'); ?>
-			Posted by <?php the_author(); ?></p>
-			
-			<?php the_content('Read more...'); ?>
-			
-			<!-- Begin MailChimp Signup Form -->
-									
-				<div id="mc_embed_signup">
-					<form action="http://adarna.us3.list-manage2.com/subscribe/post?u=52208460643c2b191e0f68272&amp;id=493cffffec" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
-						<label for="mce-EMAIL">Join our community!</label>
-						<input style="margin:0 auto;"type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="email address" required>
-						<!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
-						<div style="position: absolute; left: -5000px;"><input type="text" name="b_52208460643c2b191e0f68272_493cffffec" value=""></div>
-						<div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
-					</form>
-				</div>
+			<br><br>
+			<div>
+				<h2 style="text-align:center"><?php the_title(); ?></h2>
+				
+				<p style="font-size:.7em;text-align:center;">Posted on: <?php the_time('F j, Y'); ?>
+				Posted by <?php the_author(); ?></p>
+				
+				<?php the_content('Read more...'); ?>
+				
+				<!-- Begin MailChimp Signup Form -->
+										
+					<div id="mc_embed_signup">
+						<form action="http://adarna.us3.list-manage2.com/subscribe/post?u=52208460643c2b191e0f68272&amp;id=493cffffec" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+							<label for="mce-EMAIL">Join our community!</label>
+							<input style="margin:0 auto;"type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="email address" required>
+							<!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+							<div style="position: absolute; left: -5000px;"><input type="text" name="b_52208460643c2b191e0f68272_493cffffec" value=""></div>
+							<div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
+						</form>
+					</div>
 
-			<!--End mc_embed_signup-->
-			
-			<hr class="bloghr">
-			<?php endwhile;?>
-			<?php else: ?>
-			<p> No posts here ;). </p>
-			<?php endif; ?>
+				<!--End mc_embed_signup-->
+				
+				<hr class="bloghr">
+				<?php endwhile;?>
+				<?php else: ?>
+				<p> No posts here ;). </p>
+				<?php endif; ?>
+			</div>
 	</div>
 </div>
 
