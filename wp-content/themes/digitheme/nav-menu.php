64e4cf4c8355b367fff3bda23394e7382c86b972
Agno Almario <agno@adarna.com.ph>
2014-11-03 13:51:49 +0800

    support link fix

--- wp-content/themes/digitheme/nav-menu.php
+++ wp-content/themes/digitheme/nav-menu.php
@@ -4,6 +4,6 @@
 				<li><a href="<?php bloginfo('url'); ?>/products"> Products </a></li>
 				<li><a href="<?php bloginfo('url'); ?>/blog"> Blog </a></li>
 				<li><a href="<?php bloginfo('url'); ?>/about"> About </a></li>
-				<li><a href="adarnadigital.desk.com"> Support </a></li>
+				<li><a href="https://adarnadigital.desk.com/"> Support </a></li>
 			</ul>
 		</nav>
